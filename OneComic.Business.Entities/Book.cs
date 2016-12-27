b1dbc1ccfb45b442863ea60bee17d871d17b5ff8
likeleon <likeleon@gmail.com>
2016-12-27 23:55:43 +0900

    Created solution and entity projects Business and client entities

---
+++ OneComic.Business.Entities/Book.cs
@@ -0,0 +1,23 @@
+﻿using System.Runtime.Serialization;
+
+namespace OneComic.Business.Entities
+{
+    [DataContract]
+    public sealed class Book
+    {
+        [DataMember]
+        public int BookId { get; set; }
+
+        [DataMember]
+        public string Title { get; set; }
+
+        [DataMember]
+        public string Description { get; set; }
+
+        [DataMember]
+        public string Author { get; set; }
+
+        [DataMember]
+        public string Translator { get; set; }
+    }
+}
---
+++ OneComic.Client.Entities/Book.cs
@@ -0,0 +1,41 @@
+﻿namespace OneComic.Client.Entities
+{
+    public sealed class Book
+    {
+        private int _bookId;
+        private string _title;
+        private string _description;
+        private string _author;
+        private string _translator;
+
+        public int BookId
+        {
+            get { return _bookId; }
+            set { _bookId = value; }
+        }
+
+        public string Title
+        {
+            get { return _title; }
+            set { _title = value; }
+        }
+
+        public string Description
+        {
+            get { return _description; }
+            set { _description = value; }
+        }
+
+        public string Author
+        {
+            get { return _author; }
+            set { _author = value; }
+        }
+
+        public string Translator
+        {
+            get { return _translator; }
+            set { _translator = value; }
+        }
+    }
+}
