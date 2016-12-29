6b09e6b14a754ff7f16b3178b84f4905333d8f9c
Sung Ho Kim <likeleon@gmail.com>
2016-12-29 17:10:40 +0900

    Setting up the ORM

---
+++ OneComic.Data/OneComicContext.cs
@@ -0,0 +1,33 @@
+﻿using Core.Common.Contracts;
+using OneComic.Business.Entities;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Runtime.Serialization;
+
+namespace OneComic.Data
+{
+    public class OneComicContext : DbContext
+    {
+        public OneComicContext()
+            : base("name=OneComic")
+        {
+            Database.SetInitializer<OneComicContext>(null);
+        }
+
+        public DbSet<Account> AccountSet { get; set; }
+        public DbSet<Book> BookSet { get; set; }
+        public DbSet<Bookmark> BookmarkSet { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+
+            modelBuilder.Ignore<ExtensionDataObject>();
+            modelBuilder.Ignore<IIdentifiableEntity>();
+
+            modelBuilder.Entity<Account>().HasKey(a => a.AccountId).Ignore(a => a.EntityId);
+            modelBuilder.Entity<Book>().HasKey(b => b.BookId).Ignore(b => b.EntityId);
+            modelBuilder.Entity<Bookmark>().HasKey(b => b.BookmarkId).Ignore(b => b.EntityId);
+        }
+    }
+}
