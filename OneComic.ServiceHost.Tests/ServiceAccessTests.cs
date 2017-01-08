b7053846b2e2daf9460499605db13167cdf14d5e
likeleon <likeleon@gmail.com>
2017-01-08 14:45:06 +0900

    Testing service connectivity

---
+++ OneComic.ServiceHost.Tests/ServiceAccessTests.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OneComic.Business.Contracts;
+using System.ServiceModel;
+
+namespace OneComic.ServiceHost.Tests
+{
+    [TestClass]
+    public class ServiceAccessTests
+    {
+        [TestMethod]
+        public void TestLibraryManagerAsService()
+        {
+            var channelFactory = new ChannelFactory<ILibraryService>("");
+            var proxy = channelFactory.CreateChannel();
+            (proxy as ICommunicationObject).Open();
+            channelFactory.Close();
+        }
+
+        [TestMethod]
+        public void TestBookmarkManagerAsService()
+        {
+            var channelFactory = new ChannelFactory<IBookmarkService>("");
+            var proxy = channelFactory.CreateChannel();
+            (proxy as ICommunicationObject).Open();
+            channelFactory.Close();
+        }
+
+        [TestMethod]
+        public void TestAccountManagerAsService()
+        {
+            var channelFactory = new ChannelFactory<IAccountService>("");
+            var proxy = channelFactory.CreateChannel();
+            (proxy as ICommunicationObject).Open();
+            channelFactory.Close();
+        }
+    }
+}
