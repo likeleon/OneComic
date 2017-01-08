4404ea5a936420bc175958203971df1f37bc2296
likeleon <likeleon@gmail.com>
2017-01-08 15:21:03 +0900

    Setting up the client-side contracts

---
+++ OneComic.Client.Contracts/Service Contracts/IAccountService.cs
@@ -0,0 +1,29 @@
+﻿using Core.Common.Contracts;
+using Core.Common.Exceptions;
+using OneComic.Client.Entities;
+using OneComic.Common;
+using System.ServiceModel;
+using System.Threading.Tasks;
+
+namespace OneComic.Client.Contracts
+{
+    [ServiceContract]
+    public interface IAccountService : IServiceContract
+    {
+        [OperationContract]
+        [FaultContract(typeof(NotFoundException))]
+        [FaultContract(typeof(AuthorizationValidationException))]
+        Account GetAccount(string loginEmail);
+
+        [OperationContract]
+        [FaultContract(typeof(AuthorizationValidationException))]
+        [TransactionFlow(TransactionFlowOption.Allowed)]
+        void UpdateAccount(Account account);
+
+        [OperationContract]
+        Task<Account> GetAccountAsync(string loginEmail);
+
+        [OperationContract]
+        Task UpdateAccountAsync(Account account);
+    }
+}
---
+++ OneComic.Client.Contracts/Service Contracts/IBookmarkService.cs
@@ -0,0 +1,40 @@
+﻿using Core.Common.Contracts;
+using Core.Common.Exceptions;
+using OneComic.Client.Entities;
+using OneComic.Common;
+using System.ServiceModel;
+using System.Threading.Tasks;
+
+namespace OneComic.Client.Contracts
+{
+    [ServiceContract]
+    public interface IBookmarkService : IServiceContract
+    {
+        [OperationContract]
+        [TransactionFlow(TransactionFlowOption.Allowed)]
+        [FaultContract(typeof(NotFoundException))]
+        [FaultContract(typeof(PageNumberOutOfRangeException))]
+        [FaultContract(typeof(AuthorizationValidationException))]
+        Bookmark AddBookmark(string loginEmail, int bookId, int pageNumber);
+
+        [OperationContract]
+        [TransactionFlow(TransactionFlowOption.Allowed)]
+        [FaultContract(typeof(NotFoundException))]
+        [FaultContract(typeof(AuthorizationValidationException))]
+        void RemoveBookmark(int bookmarkId);
+
+        [OperationContract]
+        [FaultContract(typeof(NotFoundException))]
+        [FaultContract(typeof(AuthorizationValidationException))]
+        AccountBookmarkData[] GetBookmarks(string loginEmail);
+
+        [OperationContract]
+        Task<Bookmark> AddBookmarkAsync(string loginEmail, int bookId, int pageNumber);
+
+        [OperationContract]
+        Task RemoveBookmarkAsync(int bookmarkId);
+
+        [OperationContract]
+        Task<AccountBookmarkData[]> GetBookmarksAsync(string loginEmail);
+    }
+}
