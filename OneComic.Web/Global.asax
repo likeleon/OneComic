8ff315a56a9b252d22c12f6d42630bcbee50b336
likeleon <likeleon@gmail.com>
2017-01-10 23:19:55 +0900

    OneComic.Web with MEF bootrstrapping

---
+++ OneComic.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="OneComic.Web.MvcApplication" Language="C#" %>
---
+++ OneComic.Web/Global.asax.cs
@@ -0,0 +1,30 @@
+﻿using OneComic.Client.Bootstrapper;
+using OneComic.Web.Core;
+using System.ComponentModel.Composition.Hosting;
+using System.Reflection;
+using System.Web.Http;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+
+namespace OneComic.Web
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            var catalog = new AggregateCatalog();
+            catalog.Catalogs.Add(new AssemblyCatalog(Assembly.GetExecutingAssembly()));
+            var container = MefLoader.Init(catalog.Catalogs);
+
+            DependencyResolver.SetResolver(new MefDependencyResolver(container)); // view controllers
+            GlobalConfiguration.Configuration.DependencyResolver = new MefAPIDependencyResolver(container); // web api controller
+        }
+    }
+}
