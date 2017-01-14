976c227ccdfad1f680d77c8790e5bf8d0b53160f
likeleon <likeleon@gmail.com>
2017-01-14 18:19:28 +0900

    Web layout

---
+++ OneComic.Web/Controllers/MVC/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.Composition;
+using System.Web.Mvc;
+
+namespace OneComic.Web.Controllers
+{
+    [Export]
+    [PartCreationPolicy(CreationPolicy.NonShared)]
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
