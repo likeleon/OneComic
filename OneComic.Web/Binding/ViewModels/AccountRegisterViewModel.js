0e9eac8ab8ee6a407c0d21f551fe06b95effae02
likeleon <likeleon@gmail.com>
2017-01-17 00:09:51 +0900

    Account registration

--- OneComic.Web/Binding/ViewModels/AccountRegisterViewModel.js
+++ OneComic.Web/Binding/ViewModels/AccountRegisterViewModel.js
@@ -4,10 +4,13 @@
     $routeProvider.when(OneComic.rootPath + 'account/register/step2', { templateUrl: OneComic.rootPath + 'Templates/RegisterStep2.html', controller: 'AccountRegisterStep2ViewModel' });
     $routeProvider.when(OneComic.rootPath + 'account/register/confirm', { templateUrl: OneComic.rootPath + 'Templates/RegisterConfirm.html', controller: 'AccountRegisterConfirmViewModel' });
     $routeProvider.otherwise({ redirectTo: OneComic.rootPath + 'account/register/step1' });
-    $locationProvider.html5Mode(true);
+    $locationProvider.html5Mode({
+      enabled: true,
+      requireBase: false
+    });
   });
 
-accountRegisterModule.controller('AccountRegisterViewModel', function ($scope, $http, $location, $window, viewModelHelper) {
+accountRegisterModule.controller("AccountRegisterViewModel", function ($scope, $http, $location, $window, viewModelHelper) {
 
   $scope.viewModelHelper = viewModelHelper;
   
@@ -21,12 +24,97 @@ accountRegisterModule.controller('AccountRegisterViewModel', function ($scope, $
 
 accountRegisterModule.controller("AccountRegisterStep1ViewModel", function ($scope, $http, $location, $window, viewModelHelper, validator) {
 
+  viewModelHelper.modelIsValid = true;
+  viewModelHelper.modelErrors = [];
+
+  var accountModelStep1Rules = [];
+
+  var setupRules = function () {
+    accountModelStep1Rules.push(new validator.PropertyRule("FirstName", {
+      required: { message: "First name is required" }
+    }));
+    accountModelStep1Rules.push(new validator.PropertyRule("LastName", {
+      required: { message: "Last name is required" }
+    }));
+  }
+
+  $scope.step2 = function () {
+    validator.ValidateModel($scope.accountModelStep1, accountModelStep1Rules);
+    viewModelHelper.modelIsValid = $scope.accountModelStep1.isValid;
+    viewModelHelper.modelErrors = $scope.accountModelStep1.errors;
+    if (viewModelHelper.modelIsValid) {
+      viewModelHelper.apiPost('api/account/register/validate1', $scope.accountModelStep1,
+        function (result) {
+          $scope.accountModelStep1.Initialized = true;
+          $location.path(OneComic.rootPath + 'account/register/step2');
+        });
+    }
+  }
+
+  setupRules();
 });
 
 accountRegisterModule.controller("AccountRegisterStep2ViewModel", function ($scope, $http, $location, $window, viewModelHelper, validator) {
 
+  if (!$scope.accountModelStep1.Initialized) {
+    $location.path(OneComic.rootPath + 'account/register/step1');
+  }
+
+  viewModelHelper.modelIsValid = true;
+  viewModelHelper.modelErrors = [];
+
+  var accountModelStep2Rules = [];
+
+  var setupRules = function () {
+    accountModelStep2Rules.push(new validator.PropertyRule("LoginEmail", {
+      required: { message: "Login Email is required" },
+    }));
+    accountModelStep2Rules.push(new validator.PropertyRule("Password", {
+      required: { message: "Password name is required" },
+      minLength: { message: "Password must be at least 6 characters", params: 6 }
+    }));
+    accountModelStep2Rules.push(new validator.PropertyRule("PasswordConfirm", {
+      required: { message: "Password confirmation is required" },
+      custom: {
+        validator: OneComic.mustEqual,
+        message: "Password do not match",
+        params: function () { return $scope.accountModelStep2.Password; }
+      }
+    }));
+  }
+
+  $scope.confirm = function () {
+    validator.ValidateModel($scope.accountModelStep2, accountModelStep2Rules);
+    viewModelHelper.modelIsValid = $scope.accountModelStep2.isValid;
+    viewModelHelper.modelErrors = $scope.accountModelStep2.errors;
+    if (viewModelHelper.modelIsValid) {
+      viewModelHelper.apiPost('api/account/register/validate2', $scope.accountModelStep2,
+        function (result) {
+          $scope.accountModelStep2.Initialized = true;
+          $location.path(OneComic.rootPath + 'account/register/confirm');
+        });
+    }
+  }
+
+  setupRules();
 });
 
 accountRegisterModule.controller("AccountRegisterConfirmViewModel", function ($scope, $http, $location, $window, viewModelHelper, validator) {
 
+  if (!$scope.accountModelStep2.Initialized) {
+    $location.path(OneComic.rootPath + 'account/register/step1');
+  }
+
+  $scope.createAccount = function () {
+
+    var accountModel;
+
+    accountModel = $.extend(accountModel, $scope.accountModelStep1);
+    accountModel = $.extend(accountModel, $scope.accountModelStep2);
+
+    viewModelHelper.apiPost('api/account/register', accountModel,
+      function (result) {
+        $window.location.href = OneComic.rootPath;
+      });
+  }
 });
--- OneComic.Web/Controllers/API/AccountApiController.cs
+++ OneComic.Web/Controllers/API/AccountApiController.cs
@@ -1,5 +1,6 @@
 ﻿using OneComic.Web.Core;
 using OneComic.Web.Models;
+using System.Collections.Generic;
 using System.ComponentModel.Composition;
 using System.Net;
 using System.Net.Http;
@@ -36,6 +37,76 @@ namespace OneComic.Web.Controllers
                     response = request.CreateErrorResponse(HttpStatusCode.Unauthorized, "Unauthorized login.");
 
                 return response;
+            });
+        }
+
+        [HttpPost]
+        [Route("register/validate1")]
+        public HttpResponseMessage ValidateRegistrationStep1(HttpRequestMessage request, [FromBody]AccountRegisterModel accountModel)
+        {
+            return GetHttpResponse(request, () =>
+            {
+                var errors = new List<string>();
+
+                if (string.IsNullOrWhiteSpace(accountModel.FirstName))
+                    errors.Add("First name is required");
+
+                if (string.IsNullOrWhiteSpace(accountModel.LastName))
+                    errors.Add("Last name is required");
+
+                return CreateResponseMessage(request, errors);
+            });
+        }
+
+        private static HttpResponseMessage CreateResponseMessage(HttpRequestMessage request, List<string> errors)
+        {
+            if (errors.Count == 0)
+                return request.CreateResponse(HttpStatusCode.OK);
+            else
+                return request.CreateResponse(HttpStatusCode.BadRequest, errors.ToArray());
+        }
+
+        [HttpPost]
+        [Route("register/validate2")]
+        public HttpResponseMessage ValidateRegistrationStep2(HttpRequestMessage request, [FromBody]AccountRegisterModel accountModel)
+        {
+            return GetHttpResponse(request, () =>
+            {
+                var errors = new List<string>();
+
+                if (_securityAdapter.UserExists(accountModel.LoginEmail))
+                    errors.Add("An account is already registered with this email address.");
+
+                if (accountModel.Password == null || accountModel.Password.Length < 6)
+                    errors.Add("Password must be at least 6 characters");
+
+                return CreateResponseMessage(request, errors);
+            });
+        }
+
+        [HttpPost]
+        [Route("register")]
+        public HttpResponseMessage CreateAccount(HttpRequestMessage request, [FromBody]AccountRegisterModel accountModel)
+        {
+            return GetHttpResponse(request, () =>
+            {
+                var response = ValidateRegistrationStep1(request, accountModel);
+                if (!response.IsSuccessStatusCode)
+                    return response;
+
+                response = ValidateRegistrationStep2(request, accountModel);
+                if (!response.IsSuccessStatusCode)
+                    return response;
+
+                _securityAdapter.Register(accountModel.LoginEmail, accountModel.Password,
+                    propertyValues: new
+                    {
+                        FirstName = accountModel.FirstName,
+                        LastName = accountModel.LastName,
+                    });
+                _securityAdapter.Login(accountModel.LoginEmail, accountModel.Password, rememberMe: false);
+
+                return request.CreateResponse(HttpStatusCode.OK);
             });
         }
     }
