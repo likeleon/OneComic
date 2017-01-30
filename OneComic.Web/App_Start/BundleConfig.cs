263b531d1396861e279d0c7d1fd99cbbb3ee2ee3
likeleon <likeleon@gmail.com>
2017-01-30 17:14:32 +0900

    bootstrap cyborg theme (from bootswatch) jquery ui removed

--- OneComic.Web/App_Start/BundleConfig.cs
+++ OneComic.Web/App_Start/BundleConfig.cs
@@ -10,25 +10,8 @@ namespace OneComic.Web
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
 
-            bundles.Add(new ScriptBundle("~/bundles/jqueryui").Include(
-                        "~/Scripts/jquery-ui-{version}.js"));
-
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
-
-            bundles.Add(new StyleBundle("~/Content/themes/base/css").Include(
-                "~/Content/themes/base/core.css",
-                "~/Content/themes/base/resizable.css",
-                "~/Content/themes/base/selectable.css",
-                "~/Content/themes/base/accordion.css",
-                "~/Content/themes/base/autocomplete.css",
-                "~/Content/themes/base/button.css",
-                "~/Content/themes/base/dialog.css",
-                "~/Content/themes/base/slider.css",
-                "~/Content/themes/base/tabs.css",
-                "~/Content/themes/base/datepicker.css",
-                "~/Content/themes/base/progressbar.css",
-                "~/Content/themes/base/theme.css"));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
@@ -40,9 +23,7 @@ namespace OneComic.Web
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/metro-bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/bootstrap.cyborg.css"));
 
             // Set EnableOptimizations to false for debugging. For more information,
             // visit http://go.microsoft.com/fwlink/?LinkId=301862
