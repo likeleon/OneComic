7af3f567397b2ead0df673438197cfb3e9b707cc
likeleon <likeleon@gmail.com>
2017-01-31 23:38:46 +0900

    Prototyping comic list view

---
+++ OneComic.Web/Binding/ViewModels/ComicListViewModel.js
@@ -0,0 +1,10 @@
+﻿appMainModule.controller("ComicListViewModel", function ($scope) {
+  $scope.comics = [];
+  for (var i = 0; i < 10; ++i) {
+    var comic = new Object();
+    comic.CoverImage = "https://media.manaa.space/comics/3627/thumbnail.jpeg";
+    comic.Title = "사카모토입니다만?"
+    comic.Description = "사노 나미, 학원/드라마/개그";
+    $scope.comics.push(comic);
+  }
+});
