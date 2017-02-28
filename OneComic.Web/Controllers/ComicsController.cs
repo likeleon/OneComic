3da7f9ab399e23467e3e6d6cfd07eb33697872f4
Sung Ho Kim <likeleon@gmail.com>
2017-02-28 21:02:51 +0900

    Comics MVC controller

---
+++ OneComic.Web/Controllers/ComicsController.cs
@@ -0,0 +1,31 @@
+﻿using Newtonsoft.Json;
+using OneComic.Data.DTO;
+using OneComic.Web.Core;
+using OneComic.Web.Helpers;
+using OneComic.Web.Models;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace OneComic.Web.Controllers
+{
+    [Export]
+    [PartCreationPolicy(CreationPolicy.NonShared)]
+    [RoutePrefix("comics")]
+    public class ComicsController : ViewControllerBase
+    {
+        public async Task<ActionResult> Index()
+        {
+            var client = OneComicHttpClient.Instance;
+
+            var response = await client.GetAsync("api/comics");
+            if (!response.IsSuccessStatusCode)
+                return Content("An error occured.");
+
+            var content = await response.Content.ReadAsStringAsync();
+            var comics = JsonConvert.DeserializeObject<IEnumerable<Comic>>(content);
+            return View(new ComicsViewModel { Comics = comics });
+        }
+    }
+}
---
+++ OneComic.Web/Models/ComicsViewModel.cs
@@ -0,0 +1,10 @@
+﻿using OneComic.Data.DTO;
+using System.Collections.Generic;
+
+namespace OneComic.Web.Models
+{
+    public class ComicsViewModel
+    {
+        public IEnumerable<Comic> Comics { get; set; }
+    }
+}
