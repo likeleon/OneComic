a9cf5834f38848c4b7800f321474a06ebc8de4ce
likeleon <likeleon@gmail.com>
2017-03-14 09:06:51 +0900

    Comic DTO validation

--- OneComic.Data.DTO/Comic.cs
+++ OneComic.Data.DTO/Comic.cs
@@ -1,11 +1,44 @@
-﻿using System;
+﻿using FluentValidation;
+using OneComic.Core;
+using System;
 
 namespace OneComic.Data.DTO
 {
-    public sealed class Comic
+    public sealed class Comic : ObjectBase
     {
-        public int ComicId { get; set; }
-        public Uri CoverImageUri { get; set; }
-        public string Title { get; set; }
+        private int _comicId;
+        private Uri _coverImageUri;
+        private string _title;
+
+        public int ComicId
+        {
+            get { return _comicId; }
+            set { Set(ref _comicId, value); }
+        }
+
+        public Uri CoverImageUri
+        {
+            get { return _coverImageUri; }
+            set { Set(ref _coverImageUri, value); }
+        }
+
+        public string Title
+        {
+            get { return _title; }
+            set { Set(ref _title, value); }
+        }
+
+        private sealed class ComicValidator : AbstractValidator<Comic>
+        {
+            public ComicValidator()
+            {
+                RuleFor(comic => comic.Title).NotEmpty();
+            }
+        }
+
+        protected override IValidator CreateValidator()
+        {
+            return new ComicValidator();
+        }
     }
 }
